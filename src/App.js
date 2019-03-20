8ba68fdec396bdb4a4cd7f9bd4b7ae7252f4b578
irosgrim <irosgrim@gmail.com>
2019-03-20 12:18:24 +0100

    donut chart component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,92 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Donutchart from './components/Donutchart';
+import './styles/App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		const donutChart = {
+			budget: 0,
+			baseColor: '#dedede',
+			thickness: '5',
+			holeColor: '#ffffff',
+			items: [
+				{
+					label: 'Food',
+					sectionColor: 'red',
+					amount: 375,
+					startFrom: 25
+				},
+				{
+					label: 'Electricity',
+					sectionColor: 'blue',
+					amount: 375,
+					startFrom: 0
+				},
+				{
+					label: 'Water',
+					sectionColor: 'orange',
+					amount: 375,
+					startFrom: 0
+				},
+				{
+					label: 'Fun',
+					sectionColor: 'yellow',
+					amount: 300,
+					startFrom: 0
+				},
+				{
+					label: 'Cats',
+					sectionColor: 'black',
+					amount: 25,
+					startFrom: 0
+				}
+			]
+		};
+
+		const donutChart2 = {
+			budget: 10000, //percentage of each item will be based on this number if set and if it's greater than the total of items
+			baseColor: '#dadada', //this is the 100% base circle
+			thickness: '5', // circle size
+			holeColor: 'transparent', //the hole is transparent but it can be any color ex #ffffff
+			items: [
+				{
+					label: 'Wolves', //label of the item
+					sectionColor: 'green', //color of the sections
+					amount: 666, //value of the section
+					startFrom: 25 //first item should always have a startFrom value > 0, next items will have a startFrom 0
+				},
+				{
+					label: 'Cats',
+					sectionColor: 'red',
+					amount: 127,
+					startFrom: 0
+				},
+				{
+					label: 'Hamsters',
+					sectionColor: '#345abc',
+					amount: 892,
+					startFrom: 0
+				},
+				{
+					label: 'Deers',
+					sectionColor: 'pink',
+					amount: 892,
+					startFrom: 0
+				}
+			]
+		};
+
+		return (
+			<div className="App">
+				<div className="chart">
+					<Donutchart chartfrom={donutChart} />
+				</div>
+				<div className="chart">
+					<Donutchart chartfrom={donutChart2} />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/Donutchart.jsx
@@ -0,0 +1,157 @@
+import React, { Component } from 'react';
+
+/* 
+Inspired by: https://medium.com/@heyoka/scratch-made-svg-donut-pie-charts-in-html5-2c587e935d72 by Mark Caron
+
+Each circle strokeDashoffset depends on the previous circle and can be calculated like this:
+Circumference - previous_circle_length + previous_circle_strokeDashoffset
+
+                100 - stroke-dasharray[0] + previousCircle.strokeDashoffset
+
+*/
+class Donutchart extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			info: 'food'
+		};
+		this.handleSectionSize = this.handleSectionSize.bind(this);
+	}
+
+	handleSectionSize(amount) {
+		const amountarr = [];
+		for (let val in this.props.chartfrom.items) {
+			amountarr.push(this.props.chartfrom.items[val].amount);
+		}
+		const reducer = (tot, current_val) => {
+			return (tot += current_val);
+		};
+
+		const total =
+			this.props.chartfrom.budget !== 0 &&
+			this.props.chartfrom.budget !== null &&
+			this.props.chartfrom.budget !== undefined &&
+			this.props.chartfrom.budget > amountarr.reduce(reducer)
+				? this.props.chartfrom.budget
+				: amountarr.reduce(reducer);
+		const slice = ((100 * amount) / total).toFixed(1);
+		const arr = slice + ' ' + (100 - slice);
+		return arr;
+	}
+	render() {
+		const { baseColor, thickness, holeColor } = this.props.chartfrom;
+		//make a copy of the items array props
+		const circleArr = JSON.parse(JSON.stringify(this.props.chartfrom.items));
+		let sections = [];
+		//calculate section size for each item
+		for (let key in circleArr) {
+			if (circleArr.hasOwnProperty(key)) {
+				circleArr[key].amount = this.handleSectionSize(circleArr[key].amount);
+				if (key > 0) {
+					//calculate section position based on the first section
+					circleArr[key].startFrom =
+						100 -
+						parseFloat(circleArr[key - 1].amount.split(' ')[0]) +
+						circleArr[key - 1].startFrom;
+				}
+				sections = [...sections, circleArr[key]];
+			}
+		}
+
+		console.log(this.props.chartfrom.items[0].amount);
+
+		const circles = sections.map((circle, index) => {
+			return (
+				<circle
+					key={index}
+					id={circle.label + '_' + index}
+					className="donut-segment"
+					cx="21"
+					cy="21"
+					r="15.91549430918954"
+					fill="transparent"
+					stroke={circle.sectionColor}
+					strokeWidth="5"
+					strokeDasharray={circle.amount}
+					strokeDashoffset={circle.startFrom}
+				/>
+			);
+		});
+		const amountarr = [];
+
+		for (let val in this.props.chartfrom.items) {
+			amountarr.push(this.props.chartfrom.items[val].amount);
+		}
+		const total = (tot, current_val) => {
+			return (tot += current_val);
+		};
+
+		return (
+			<div className="donut-chart">
+				<svg width="100%" height="100%" viewBox="0 0 42 42" className="donut">
+					<circle
+						className="donut-hole"
+						cx="21"
+						cy="21"
+						r="15.91549430918954"
+						fill={holeColor}
+					/>
+					<circle
+						className="donut-ring base"
+						cx="21"
+						cy="21"
+						r="15.91549430918954"
+						fill="transparent"
+						stroke={baseColor}
+						strokeWidth={thickness}
+					/>
+					{circles}
+
+					<g className="main-label">
+						<text x="50%" y="50%" dx="-1px">
+							{this.props.chartfrom.budget < amountarr.reduce(total)
+								? amountarr.reduce(total)
+								: this.props.chartfrom.budget}
+						</text>
+					</g>
+				</svg>
+				<div className="info">
+					<ul className="info-list" style={{ listStyleType: 'none' }}>
+						{sections.map((item, index) => {
+							return (
+								<li
+									key={index}
+									style={{ display: 'flex', alignItems: 'center' }}>
+									<div
+										style={{
+											float: 'left',
+											height: '10px',
+											width: '10px',
+											marginRight: '10px',
+											borderRadius: ' 3px',
+											backgroundColor: item.sectionColor
+										}}
+									/>
+
+									{item.label +
+										' ' +
+										this.props.chartfrom.items[index].amount +
+										` (${item.amount.split(' ')[0]}%)`}
+								</li>
+							);
+						})}
+						<li style={{ display: 'flex', alignItems: 'center' }}>
+							TOTAL:{' '}
+							{this.props.chartfrom.budget < amountarr.reduce(total)
+								? amountarr.reduce(total)
+								: amountarr.reduce(total) +
+								  ' out of ' +
+								  this.props.chartfrom.budget}
+						</li>
+					</ul>
+				</div>
+			</div>
+		);
+	}
+}
+export default Donutchart;
